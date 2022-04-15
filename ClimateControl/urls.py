804acfa03e9370d9cadf92681c23d362fb563db4
CHIH-WEI HSU <kevin@Kevin-teki-MBP.attlocal.net>
2022-04-14 21:26:55 -0500

    User listing and creating

--- ClimateControl/urls.py
+++ ClimateControl/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from .views import ResetPasswordView, IndexView
+from .views import ResetPasswordView, IndexView, UserListView, UserCreateView
 from .forms import LoginForm
 
 urlpatterns = [
@@ -8,4 +8,6 @@ urlpatterns = [
     path('login/', LoginView.as_view(form_class=LoginForm, template_name='login.html'), name='Login'),
     path('logout/', LogoutView.as_view(), name='Logout'),
     path('reset/', ResetPasswordView.as_view(template_name='password_reset.html'), name='PasswordReset'),
+    path('user/', UserListView.as_view(), name='UserList'),
+    path('user/create/', UserCreateView.as_view(), name='UserCreate'),
 ]
--- ClimateControl/views.py
+++ ClimateControl/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.views import PasswordChangeView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import get_user_model
 from django.contrib import messages
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
-from django.views.generic import TemplateView
+from django.views.generic import ListView, CreateView, TemplateView
 from .forms import ResetPasswordForm
 
 
@@ -21,3 +22,18 @@ class ResetPasswordView(PasswordChangeView):
 
 class IndexView(LoginRequiredMixin, TemplateView):
     template_name = 'index.html'
+
+
+class UserListView(ListView):
+    model = get_user_model()
+    template_name = 'UserList.html'
+
+
+class UserCreateView(CreateView):
+    model = get_user_model()
+    template_name = 'UserFrom.html'
+    fields = ['username', 'password', 'is_admin']
+
+    def get_success_url(self):
+        messages.success(self.request, _('User created'))
+        return reverse_lazy('UserList')
